9a868264728fa2b6554c9f45ee8e05f2e58556fd
jhontrujilloserrano <jhontruse@gmail.com>
2026-07-27 10:22:05 -0500

    Se creó el proyecto base

---
+++ src/main/java/com/jhontruse/biblioteca/WsrBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.jhontruse.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsrBibliotecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsrBibliotecaApplication.class, args);
+    }
+
+}
